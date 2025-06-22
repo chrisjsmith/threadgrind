a4b7d966e1e54a28dfc3e797c96e9c8382050e8a
Chris Smith <me@cjsmith.uk>
2025-06-22 18:27:36 +0100

    sort by result

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #define _GNU_SOURCE
 #include <pthread.h>
 #include <sched.h>
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -63,6 +64,17 @@ void *cpu_worker(void *p) {
 	return NULL;
 }
 
+int grindt_compare_result(const void* a, const void* b) {
+	grindt *grindt_a = (grindt *)a;
+	grindt *grindt_b = (grindt *)b;
+
+	if (grindt_a->result < grindt_b->result)
+		return -1;
+	if (grindt_a->result > grindt_b->result)
+		return 1;
+	return 0;
+}
+
 int main(int argc, char *argv[]) {
 	int i, nprocs, nthreads;
 
@@ -86,6 +98,9 @@ int main(int argc, char *argv[]) {
 	}
        	for (i = 0; i < nthreads; i++) 
 		pthread_join(threads[i], NULL);	
+
+	/* sort */
+	qsort(tmap, nthreads, sizeof(grindt), grindt_compare_result);
 
 	/* print results */
 	for (i = 0; i < nthreads; i++)
